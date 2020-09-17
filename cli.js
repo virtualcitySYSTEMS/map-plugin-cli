ee42b886cb910970c0c38af112b6bbfc507b8890
Ben Kuster <bkuster@virtualcitysystems.de>
2020-09-17 13:30:14 +0200

    added default command handling

--- cli.js
+++ cli.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const program = require('commander');
+require('./src/defaultCommand');
 const { version } = require('./package.json');
 const { serve, build, compile } = require('./index');
 
@@ -7,24 +8,20 @@ program.version(version);
 
 program
   .command('build')
-  .option('-n, --plugin-name [name]', 'a name override to use. extracts the name from the package.json by default')
-  .option('--no-condense-whitespace', 'do not condense white space')
+  .defaultBuildOptions()
   .action(build);
 
 program
   .command('serve')
-  .option('-n, --plugin-name [name]', 'a name override to use. extracts the name from the package.json by default')
+  .defaultOptions()
   .option('-p, --port [port]', 'the port to listen on', /\d+/, '8080')
   .option('--vcm [dir]', 'the directory path or URL to a virtualcityMAP application', './vcm')
   .action(serve);
 
 program
   .command('compile')
-  .option('-n, --plugin-name [name]', 'a name override to use. extracts the name from the package.json by default')
-  .option('--no-condense-whitespace', 'do not condense white space')
-  .option('-l, --library [name]', 'whether to create a library with [name] or not')
-  .option('--library-target', 'library target', 'commonjs2')
-  .option('-e, --entry <entry>', 'entrypoint override')
+  .defaultBuildOptions()
+  .option('--no-modern', 'build for legacy')
   .action(compile);
 
 program.parse(process.argv);
---
+++ src/defaultCommand.js
@@ -0,0 +1,19 @@
+const { Command } = require('commander');
+
+Command.prototype.defaultOptions = function defaultOptions() {
+  this
+    .option('-n, --plugin-name [name]', 'a name override to use. extracts the name from the package.json by default')
+    .option('--no-condense-whitespace', 'do not condense white space')
+    .option('-e, --entry <entry>', 'entrypoint override');
+
+  return this;
+};
+
+Command.prototype.defaultBuildOptions = function defaultBuildOptions() {
+  this
+    .defaultOptions()
+    .option('-l, --library [name]', 'whether to create a library with [name] or not')
+    .option('--library-target', 'library target', 'commonjs2');
+
+  return this;
+};
